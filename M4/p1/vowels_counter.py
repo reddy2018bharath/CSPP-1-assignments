36b1cf3744117146558835bf8eb181cdfcd78b83
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-02 18:01:14 +0530

    " -> 4 of 4 passed. pylint: 6.67/10 "

--- M4/p1/vowels_counter.py
+++ M4/p1/vowels_counter.py
@@ -3,11 +3,11 @@
 def main():
 
     str_ = input()
-    coun_= 0
+    coun_ = 0
     for char in str_:
         if char in ('a', 'e', 'i', 'o', 'u'):
-            coun_=coun_+ 1
+            coun_ = coun_ + 1
     print(coun_)
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	main()
