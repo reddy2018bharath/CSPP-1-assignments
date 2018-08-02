09ba6769cd80dd602c9eba98634bdc73a0f0649d
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-02 18:05:44 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- M4/p1/vowels_counter.py
+++ M4/p1/vowels_counter.py
@@ -1,10 +1,10 @@
 """To find number of vowels in a string""" 
-def main():
+ def main():
     str_ = input()
     coun_ = 0
     for char in str_:
         if char in ('a', 'e', 'i', 'o', 'u'):
             coun_ = coun_ + 1
     print(coun_)
-if __name__ == "__main__":
+ if __name__ == "__main__":
 	main()
