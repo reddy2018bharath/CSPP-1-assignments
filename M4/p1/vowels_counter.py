bc1d3a1529ad16b1bb9a97b485803565ed972162
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-02 17:54:19 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- M4/p1/vowels_counter.py
+++ M4/p1/vowels_counter.py
@@ -1,13 +1,10 @@
-#Assume s is a string of lower case characters.
+"""string""" 
 
-#Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
-
-#Number of vowels: 5
-
-def main():
-    str = input()
+def=main():
+        """string"""
+    str_= input()
     coun_= 0
-    for char in str:
+    for char in str_:
         if char in ('a','e','i','o','u'):
             coun_=coun_+ 1
     print(coun_)
