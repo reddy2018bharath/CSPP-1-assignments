43d18f97bb123a8bef21c2f9cff6603d2890b56e
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-04 18:27:26 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -1,21 +1,17 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
+'''This program evaluates square root of number using bisection method.'''
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''Main function.'''
+    num_1 = int(input())
+    epsilon = 0.01
+    low = 0
+    high = num_1
+    guess = (high + low)/2.0
+    while abs(guess**2-num_1) >= epsilon:
+        if guess**2 < num_1:
+            low = guess
+        else:
+            high = guess
+        guess = (high + low)/2.0
+    print(guess)
+if __name__ == "__main__":
+    main()
