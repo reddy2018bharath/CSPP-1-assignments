53eb563d69696abb69aeaf9b771a853c43cfb992
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-04 18:30:28 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- M5/p4/square_root_newtonrapson.py
+++ M5/p4/square_root_newtonrapson.py
@@ -1,20 +1,11 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
+'''This program evaluates the square root of number using Newton Raphson method'''
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''Main function.'''
+    num_1 = int(input())
+    epsilon = 0.01
+    guess = num_1/2.0
+    while abs(guess**2-num_1) >= epsilon:
+        guess = guess-(((guess**2)-num_1)/(2*guess))
+    print(guess)
+if __name__ == "__main__":
+    main()
