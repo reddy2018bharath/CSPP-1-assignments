16bc708fe8448d42fc5bc14ee516beaecb16484a
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-04 11:33:31 +0530

    " -> 5 of 7 passed. pylint: -3.68/10 "

--- M6/p3/digit_product.py
+++ M6/p3/digit_product.py
@@ -11,11 +11,22 @@ def main():
     int_input = int(input())
     a=int_input
     c=1
-    while a>0:
-        b=a%10
-        c=c*b
-        a=a//10
-    print(c)
+    if a>0:
+        while a>0:
+            b=a%10
+            c=c*b
+            a=a//10
+        print(c)
+    elif a<0:
+        a=-a
+        while a>0:
+            b=a%10
+            c=c*b
+            a=a//10
+        print(-c)
+    
+    
+    
 
 
 if __name__ == "__main__":
