d8e513446628c16a57b5b8af04bc0bce982f38b6
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-04 18:33:02 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- M5/p1/perfect_cube.py
+++ M5/p1/perfect_cube.py
@@ -1,19 +1,13 @@
-# Write a python program to find if the given number is a perfect cube or not
-# using guess and check algorithm
-
-# testcase 1
-# Input: 24389
-# Output: 24389 is a perfect cube
-
-# testcase 2
-# Input: 21950
-# Output: 21950 is not a perfect cube
-
+'''This program evaluates whether given number is a perfect cube or not'''
 def main():
-	# input is captured in s
-	s = raw_input()
-	# watch out for the data type of value stored in s
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''Main function.'''
+    num_1 = int(input())
+    guess = 1
+    while guess**3 < num_1:
+        guess += 1
+    if guess**3 == num_1:
+        print(num_1, "is a perfect cube")
+    else:
+        print(num_1, "is not a perfect cube")
+if __name__ == "__main__":
+    main()
