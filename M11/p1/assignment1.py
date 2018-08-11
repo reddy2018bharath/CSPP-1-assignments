af5a507a63330f2c67b13e023d3ba4c875ad2312
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-11 18:31:31 +0530

    " -> 9 of 9 passed. pylint: 3.33/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -20,20 +20,17 @@ def get_word_score(word, n):
     """
     # TO DO ... <-- Remove this comment when you code this function
     
-    SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
-    }
-    sum=0
-    abc=1
-    l_1=len(word)
+    SCRABBLE_LETTER_VALUES = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,'s': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    
+    sum = 0
+    abc = 1
+    l_1 = len(word)
     for i in word:
         if i in  SCRABBLE_LETTER_VALUES:
-            sum=sum+SCRABBLE_LETTER_VALUES[i]
+            sum = sum + SCRABBLE_LETTER_VALUES[i]
         abc=sum*l_1
-        if l_1==n:
-            abc=abc+50
+        if l_1 == n:
+            abc = abc+50
     return abc
             
             
