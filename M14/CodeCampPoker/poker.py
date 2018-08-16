a46b664885bb44652dfd7e142a9db6d6384c09ab
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-16 22:39:23 +0530

    " -> 3 of 3 passed. pylint: 2.29/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -69,131 +69,6 @@ def is_flush(hand):
         return True
     else:
         return False
-def is_onepair(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=0
-    for i in b:
-        if b.count(i)==2:
-            c=c+1
-    if c==2:
-        return True
-    else:
-        return False
-def is_fourkind(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    x=0
-    c=0
-    for i in b:
-        if b.count(i)==4:
-            return True
-            #exit()
-    else:
-        return False
-def is_fullhouse(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=0
-    d=0
-    for i in b:
-        if b.count(i)==3:
-            c=1
-        if b.count(i)==2:
-            d=1
-    if c==1 and d==1:
-        return True
-    else:
-        return False
-def is_threekind(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=set(b)
-    e=len(c)
-    #print (e)
-
-    c=0
-    for i in b:
-        if b.count(i)==3:
-            c=c+1
-    if c==3 and e==3:
-        return True
-        #exit()
-    else:
-        return False
-def is_twopair(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=set(b)
-    d=len(c)
-    #print (d)
-    c=0
-    for i in b:
-        if b.count(i)==2:
-            c += 1
-    if c == 4 and d==3:
-        return True
-    else:
-        return False
-
-
-
-    
-
-        
-        
- 
-        
 
 def hand_rank(hand):
     '''
@@ -207,30 +82,6 @@ def hand_rank(hand):
     # By now you should have seen the way a card is represented.
     # If you haven't then go the main or poker function and print the hands
     # Each card is coded as a 2 character string. Example Kind of Hearts is KH
-    if is_flush(hand) and is_straight(hand):
-        return 8
-    if is_fourkind(hand):
-        return 7
-    if is_fullhouse(hand):
-        return 6
-    if is_flush(hand):
-        return 5
-    if is_straight(hand):
-        return 4
-    if is_threekind(hand):
-        return 3
-    if is_twopair(hand):
-        return 2
-    if is_onepair(hand):
-        return 1
-
-    
-
-    
-    return 0
-
-    
-    
     # First character for face value 2,3,4,5,6,7,8,9,T,J,Q,K,A
     # Second character for the suit S (Spade), H (Heart), D (Diamond), C (Clubs)
     # What would be the logic to determine if a hand is a straight or flush?
@@ -243,7 +94,13 @@ def hand_rank(hand):
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
-    
+    if is_flush(hand) and is_straight(hand):
+        return 3
+    if is_flush(hand):
+        return 2
+    if is_straight(hand):
+        return 1
+    return 0
 
 def poker(hands):
     '''
