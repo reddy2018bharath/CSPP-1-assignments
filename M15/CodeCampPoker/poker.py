25bdc2a3b5469c834ee24df34bcf6cab3ef7b1e2
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-19 18:51:52 +0530

    " -> 19 of 19 passed. pylint: 8.57/10 "

--- M15/CodeCampPoker/poker.py
+++ M15/CodeCampPoker/poker.py
@@ -5,12 +5,14 @@
 '''
 
 def kind(face_values, num):
+    """to find face values"""
     for face in face_values:
         if face_values.count(face) == num:
             return face
     
 
 def get_facevalues(hand):
+    """to find face values"""
     return sorted(['--23456789TJQKA'.index(face) for face, suite in hand], reverse = True)
 
 def is_straight(hand):
@@ -68,12 +70,12 @@ def hand_rank(hand):
     face_values = get_facevalues(hand)
     return ((8, face_values) if is_flush(hand) and is_straight(hand) else
             (7, kind(face_values, 4), face_values) if kind(face_values, 4) else
-            (6, kind(face_values, 3), kind(face_values, 2)) 
+            (6, kind(face_values, 3), kind(face_values, 2))
             if  kind(face_values, 3) and kind(face_values, 2) else
             (5, face_values) if is_flush(hand) else
             (4, face_values) if is_straight(hand) else
             (3, kind(face_values, 3), face_values) if kind(face_values, 3) else
-            (2, kind(face_values, 2), kind(sorted(face_values), 2), face_values) 
+            (2, kind(face_values, 2), kind(sorted(face_values), 2), face_values)
             if kind(face_values, 2) and kind(sorted(face_values), 2) else
             (1, kind(face_values, 2), face_values) if kind(face_values, 2) else
             (0, face_values))
