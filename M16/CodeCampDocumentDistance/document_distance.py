8840911845841035f8efd1826fd7fec1e2f86bfe
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-21 16:49:56 +0530

    " -> 1 of 3 passed. pylint: 9.76/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -1,13 +1,39 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
-
+import math
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    pass
-
+    dict1.lower()
+    dict2.lower()
+    a_dict = {}
+    word_freq = {}
+    list_1 = dict1.split(" ")
+    list_2 = dict2.split(" ")
+    list_3 = list_1 + list_2
+    spl_char = "!@#$%^&*()-_+"
+    for i in list_3:
+        if i in spl_char:
+            list_3.remove(i)
+    for i in list_3:
+        a_dict[i] = list_3.count(i)
+    dict_3 = load_stopwords("stopwords.txt")
+    for i in dict_3:
+        if i in a_dict:
+            del a_dict[i]
+    for i in list_3:
+        word_freq[i] = [list_1.count(i), list_2.count(i)]
+    numer_n = 0
+    denom_n1 = 0
+    denom_n2 = 0
+    for i in word_freq:
+        numer_n += word_freq[i][0]*word_freq[i][1]
+        denom_n1 += word_freq[i][0]**2
+        denom_n2 += word_freq[i][1]**2
+    denom_n = math.sqrt(denom_n1)*math.sqrt(denom_n2)
+    return numer_n/denom_n
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
