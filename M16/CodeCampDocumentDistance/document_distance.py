a287f8fd9017a6b7925c3da432d0af3991b63369
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-21 16:54:21 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -2,46 +2,41 @@
     Document Distance - A detailed description is given in the PDF
 '''
 import math
+import re
+def word_list(string):
+    '''function to find the word list'''
+    regex = re.compile('[^a-z]')
+    return [regex.sub("", w.strip()) for w in string.lower().split(" ")]
+def remove_stopwords(word, dict_1, stop_word, index):
+    '''function to remove stopwords'''
+    for w_1 in word:
+        if w_1 not in stop_word and len(w_1) > 0:
+            if w_1 not in dict_1.keys():
+                dict_1[w_1] = [0, 0]
+            dict_1[w_1][index] += 1
+    return dict_1
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    dict1.lower()
-    dict2.lower()
-    a_dict = {}
-    word_freq = {}
-    list_1 = dict1.split(" ")
-    list_2 = dict2.split(" ")
-    list_3 = list_1 + list_2
-    spl_char = "!@#$%^&*()-_+"
-    for i in list_3:
-        if i in spl_char:
-            list_3.remove(i)
-    for i in list_3:
-        a_dict[i] = list_3.count(i)
-    dict_3 = load_stopwords("stopwords.txt")
-    for i in dict_3:
-        if i in a_dict:
-            del a_dict[i]
-    for i in list_3:
-        word_freq[i] = [list_1.count(i), list_2.count(i)]
-    numer_n = 0
-    denom_n1 = 0
-    denom_n2 = 0
-    for i in word_freq:
-        numer_n += word_freq[i][0]*word_freq[i][1]
-        denom_n1 += word_freq[i][0]**2
-        denom_n2 += word_freq[i][1]**2
-    denom_n = math.sqrt(denom_n1)*math.sqrt(denom_n2)
-    return numer_n/denom_n
+    word_1 = word_list(dict1)
+    word_2 = word_list(dict2)
+    stop_word = load_stopwords("stopwords.txt")
+    dict_1 = {}
+    word_freq = remove_stopwords(word_1, dict_1, stop_word, 0)
+    word_freq = remove_stopwords(word_2, dict_1, stop_word, 1)
+    numer_n = sum([v[0]*v[1] for v in word_freq.values()])
+    denom_1 = math.sqrt(sum([v[0]**2 for v in word_freq.values()]))
+    denom_2 = math.sqrt(sum([v[1]**2 for v in word_freq.values()]))
+    return numer_n/(denom_1*denom_2)
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
     '''
     stopwords = {}
     with open(filename, 'r') as filename:
-        for line in filename:
-            stopwords[line.strip()] = 0
+    for line in filename:
+        stopwords[line.strip()] = 0
     return stopwords
 
 def main():
