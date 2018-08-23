a272f454211c8fd2cc6a22a3aa4de5bd93dca6cd
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-23 16:48:35 +0530

    " -> 7 of 8 passed. pylint: 3.12/10 "

--- M20/matrix_operations.py
+++ M20/matrix_operations.py
@@ -6,14 +6,14 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    result=[]
-    if (len(m1[0])==len(m2)):
+    result = []
+    if (len(m1[0]) == len(m2)):
         for i in range(len(m1)):
-            resTemp=[]
+            resTemp = []
             for j in range(len(m2[0])):
-                res=0
+                res = 0
                 for k in range(len(m2)):
-                    res+=m1[i][k]*m2[k][j]
+                    res += m1[i][k]*m2[k][j]
                 resTemp.append(res)
             result.append(resTemp)
         return result
@@ -30,16 +30,16 @@ def add_matrix(m1, m2):
         error message should be "Error: Matrix shapes invalid for addition"
     '''
     pass
-    if len(m1)!=len(m2):
+    if len(m1) != len(m2):
         print("Error: Matrix shapes invalid for addition")
         return
     for i,j in zip(m1,m2):
-        if len(i)!=len(j):
+        if len(i) != len(j):
             print("Error: Matrix shapes invalid for addition")
             return
-    result=[]
+    result = []
     for i,j in zip(m1,m2):
-        row=[]
+        row = []
         for p,q in zip(i,j):
             row.append(p+q)
         result.append(row)
@@ -56,11 +56,11 @@ def read_matrix():
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    rows,columns=[int(i) for i in input().split(",")]
-    matrix=[]
+    rows,columns = [int(i) for i in input().split(",")]
+    matrix = []
     for i in range(rows):
-        lst=[int(i) for i in input().split(" ")]
-        if len(lst)!=columns:
+        lst = [int(i) for i in input().split(" ")]
+        if len(lst) != columns:
             print("Error: Invalid input for the matrix")
             return None
         matrix.append(lst)
@@ -70,13 +70,13 @@ def read_matrix():
 
 def main():
     # read matrix 1
-    m1=read_matrix()
+    m1 = read_matrix()
 
     # read matrix 2
-    m2=read_matrix()
-    if (m1!=None and m2!=None):
-        print(add_matrix(m1,m2))
-        print(mult_matrix(m1,m2))
+    m2 = read_matrix()
+    if (m1 != None and m2 != None):
+        print(add_matrix(m1, m2))
+        print(mult_matrix(m1, m2))
         
 
     # add matrix 1 and matrix 2
