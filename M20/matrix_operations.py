875fbcd587a917a38d18ae46928dbd7b88463f5b
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-23 16:20:25 +0530

    " -> 5 of 8 passed. pylint: -0.80/10 "

--- M20/matrix_operations.py
+++ M20/matrix_operations.py
@@ -6,7 +6,6 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    pass
     result=[]
     if (len(m1[0])==len(m2)):
         for i in range(len(m1)):
