d7ccaebc2d94d70a799017276763e3045d20574c
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-25 11:25:13 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -4,12 +4,12 @@ each word
 '''
 
 def tokenize(string):
-    a_1=dict()
+    a_1 = dict()
     for i in string:
 	    if i not in a_1:
-		    a_1[i]=a_1.count(i)
+		    a_1[i] = a_1.count(i)
 	    else:
-		    a_1[i]+=1
+		    a_1[i] += 1
     return a_1
 
             
@@ -17,7 +17,7 @@ def main():
 	matrix=[]
     lines=int(input())
     for i in range(lines):
-	    lst =[i for i in input().split(" ")]
+	    lst = [i for i in input().split(" ")]
     matrix.append(lst)
 	print(tokenize(matrix))
 	
