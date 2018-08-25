2e391654a4be66c51b6c95b236fd9e1d59dbfdba
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-25 10:51:25 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- M22/assignment2/clean_input.py
+++ M22/assignment2/clean_input.py
@@ -1,16 +1,16 @@
 '''
-Write a function to clean up a given string by removing the special characters and retain 
+Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
     """to remove special characters"""
-    str = ""
+    str_1 = ""
     for i in string:
         for k in i:
             if k.isalpha() or k.isnumeric():
-                str += k
-    return str
+                str_1 += k
+    return str_1
 
 def main():
     """main function"""
